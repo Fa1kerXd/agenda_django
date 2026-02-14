4146b56ad49c9a5d920c965c879fa767165077ba
Augusto Cesar <fa1ker@icloud.com>
2026-02-14 18:06:24 -0300

    Criando Paginação

--- te.py
+++ te.py
@@ -1,9 +1,36 @@
+def separate(string):
+    texto = []
+
+    for s in range(0, len(string), 2):
+        par = string[s:s+2]
+        print(par)
+        if len(par) == 1:
+            par += "_"
+        texto.append(par)
+    return texto 
+     
 
 
-import requests
 
 
 
-url = ""
 
-request = requests.get(url,stream=True)
+
+def separate_not_slice(string):
+    resultado = []
+    par = ""
+
+    for s in string:
+        par += s
+
+        if len(par) == 2:
+            resultado.append(par)
+            par = ""
+    if len(par) == 1:
+        resultado.append(par + "_")
+    return resultado
+
+
+
+print(separate("Augusto"))
+print(separate_not_slice("Augusto"))
